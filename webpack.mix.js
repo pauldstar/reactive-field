8bfca9dad61947fb9219a6fdfeb40b8d19730173
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2020-10-16 09:47:50 +0100

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,16 @@
 let mix = require('laravel-mix')
 
 mix
-  .setPublicPath('dist')
-  .js('resources/js/field.js', 'js')
-  .sass('resources/sass/field.scss', 'css')
+    .setPublicPath('dist')
+    .js('resources/js/field.js', 'js')
+    .sass('resources/sass/field.scss', 'css')
+    .webpackConfig({
+        resolve: {
+            alias: {
+                '@nova': path.resolve(__dirname, '../../vendor/laravel/nova/resources/js/'),
+                '@ui': path.resolve(__dirname, '../nova-compact-ui/resources/js/'),
+                'laravel-nova': path.resolve(__dirname, './node_modules/laravel-nova/dist/index.js'),
+                '@': path.resolve(__dirname, '../../vendor/laravel/nova/resources/js/')
+            },
+        },
+    });
